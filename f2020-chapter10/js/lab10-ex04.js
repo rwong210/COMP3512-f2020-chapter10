2706ca3c542176ea2a55dede7e561aba6db9b472
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-17 15:33:50 -0600

    starting files

---
+++ f2020-chapter10/js/lab10-ex04.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let url = 'https://api.github.com/orgs/funwebdev-2nd-ed/repos';
+    
+    // use fetch to request data from an API
+
+
+    
+    
+});
