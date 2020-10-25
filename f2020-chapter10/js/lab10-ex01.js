1a5ee8be200b20f864ee823a139031ed255847cc
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-25 16:57:23 -0600

    finished ex01

--- f2020-chapter10/js/lab10-ex01.js
+++ f2020-chapter10/js/lab10-ex01.js
@@ -15,3 +15,61 @@ const paintings = [ {title: "Girl with a Pearl Earring", artist: "Vermeer", year
 
 /* add code here */
 
+const sortedTitles = titles.sort();
+console.log(sortedTitles);
+
+// const sortedPaintingsByYear = paintings.sort( function(a,b){
+//     if (a.year < b.year)
+//     return -1;
+//     else if (a.year > b.year)
+//     return 1;
+//     else 
+//     return 0;
+// });
+// console.log(sortedPaintingsByYear);
+
+const sortedPaintingsByYear = paintings.sort( (a,b) => {
+    return a.year < b.year ? -1 : 1;
+
+});
+
+const nineteenthCentury = paintings.filter(painting => (painting.year >= 1800 && painting.year < 1900));
+console.log(nineteenthCentury);
+
+const manet = paintings.find ( painting => painting.artist == "Manet");
+console.log (manet);
+
+paintings.forEach ( p => console.log (p.title + ' by ' + p.artist));
+
+const re = new RegExp('with', 'i');
+const regexEx = paintings.filter( p => p.title.match(re) );
+console.log(regexEx);
+
+//adds the titles array to the end of the moreTitles array
+const moreTitles = ["Balcony", "Sunflowers", ...titles];
+console.log(moreTitles);
+
+//same as above but with out spread operator (... elipses)
+const moreTitlesOldFashioned = ["Balcony", "Sunflowers"];
+for (let t of titles){
+    moreTitlesOldFashioned.push(t);
+}
+console.log(moreTitlesOldFashioned);
+
+//create a new array based on data within another array
+console.log('--- create new simple array form existing array of objects');
+const artists1 = [];
+for (let painting of paintings) {
+    artists1.push(painting.artist);
+}
+console.log(artists1);
+
+// returns an new array after performing the function on each element of the original array
+const artists2 = paintings.map( function (painting) {
+    return painting.artist;
+});
+console.log(artists2);
+
+//same as above with arrrow function
+const artists3 = paintings.map( painting => painiting.artist );
+console.log(artists3);
