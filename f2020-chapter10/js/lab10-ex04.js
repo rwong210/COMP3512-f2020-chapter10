2df95205e27dde90708bafefffded00b3becf6a7
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-25 20:24:16 -0600

    completed ex04

--- f2020-chapter10/js/lab10-ex04.js
+++ f2020-chapter10/js/lab10-ex04.js
@@ -1,9 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let url = 'https://api.github.com/orgs/funwebdev-2nd-ed/repos';
-    
+
     // use fetch to request data from an API
-
-
-    
-    
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject({
+                    status: response.status,
+                    statusText: response.statusText
+                })
+            }
+        })
+        .then(data => {
+            console.dir(data);
+        })
+    .catch(err => {
+        console.log(err)
+    });
+    console.log('after the fetch');
 });
