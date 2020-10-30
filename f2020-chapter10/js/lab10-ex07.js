70c2f35978455a5e4cc9a2d2ec702538550e3bc1
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-30 12:57:04 -0600

    starting 10.7

--- f2020-chapter10/js/lab10-ex07.js
+++ f2020-chapter10/js/lab10-ex07.js
@@ -10,4 +10,21 @@ const url = "https://www.randyconnolly.com/funwebdev/3rd/async-post.php";
 // set up button handlers here using event delegation
 document.querySelector('main').addEventListener('click', (e) => {
 
+   function showSnackBar(message) {
+      const snack = document.querySelector("#snackbar");
+      snack.textContent = message;
+      snack.classList.add("show");
+      setTimeout( () => {
+         snack.classList.remove("show");
+      }, 3000);
+   }
+   
+   document.querySelector('main').addEventListener('click', (e) => {
+      if (e.target.nodeName.toLowerCase() == 'button') {
+         // retrieve data from button and
+         let id = e.target.getAttribute('data-id');
+         // get painting object for this button
+         let p = painting.find( p => p.id ==id);
+      }
+   })
 });
