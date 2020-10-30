c8af5e4ef3608889303549b5e2210a0ab5ce4fc1
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-30 13:51:04 -0600

    finshed ex07

--- f2020-chapter10/js/lab10-ex07.js
+++ f2020-chapter10/js/lab10-ex07.js
@@ -1,30 +1,50 @@
-import { getPaintingData }  from "./paintings.js";
+import {
+   getPaintingData
+} from "./paintings.js";
 
 const main = document.querySelector('main');
 const paintings = getPaintingData();
-paintings.forEach( (p) => {
-   main.appendChild( p.render() );
+paintings.forEach((p) => {
+   main.appendChild(p.render());
 });
 const url = "https://www.randyconnolly.com/funwebdev/3rd/async-post.php";
+
+function showSnackBar(message) {
+   const snack = document.querySelector("#snackbar");
+   snack.textContent = message;
+   snack.classList.add("show");
+   setTimeout(() => {
+      snack.classList.remove("show");
+   }, 3000);
+}
 
 // set up button handlers here using event delegation
 document.querySelector('main').addEventListener('click', (e) => {
 
-   function showSnackBar(message) {
-      const snack = document.querySelector("#snackbar");
-      snack.textContent = message;
-      snack.classList.add("show");
-      setTimeout( () => {
-         snack.classList.remove("show");
-      }, 3000);
+   if (e.target.nodeName.toLowerCase() == 'button') {
+      // retrieve data from button and
+      let id = e.target.getAttribute('data-id');
+      // get painting object for this button
+      let p = paintings.find(p => p.id == id);
+
+
+      let formBody = new FormData();
+      formBody.set("id", p.id);
+      formBody.set("title", p.title);
+
+      const opt = {
+         method: 'Post',
+         body: formBody
+      };
+
+      fetch(url, opt)
+         .then(resp => resp.json())
+         .then(data => {
+            // display notification it was successful
+            showSnackBar(`${data.received.title} was added to favorites`);
+         })
+         .catch(error => {
+            showSnackBar('An error occurred, favorites not modified');
+         });
    }
-   
-   document.querySelector('main').addEventListener('click', (e) => {
-      if (e.target.nodeName.toLowerCase() == 'button') {
-         // retrieve data from button and
-         let id = e.target.getAttribute('data-id');
-         // get painting object for this button
-         let p = painting.find( p => p.id ==id);
-      }
-   })
 });
