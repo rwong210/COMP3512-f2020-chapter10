7dc28b3b15e514af57fabd076082258dc562a9b2
Ryan J. Wong <rwong210@mtroyal.ca>
2020-10-31 10:43:59 -0600

    ex12 start

--- f2020-chapter10/js/lab10-ex12.js
+++ f2020-chapter10/js/lab10-ex12.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-        
-      
-
-    
+    // document.querySelector('#speak').addEventListener('click', (e) => {
+    //     const utterance = new SpeechSynthesisUtterance('Hey there buddy');
+    //     speechSynthesis.speak(utterance);
+    // });
+    window.speechSynthesis.addEventListener('voiceschanged', () => {
+        let voices = this.getVoices();
+        voices.forEach((voice) => {
+            console.log(voice.name + " - " + voice.lang)});
+    });
 
 });
